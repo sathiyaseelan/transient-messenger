e74f8e42d91fb0f7c31448ca0bfc11d021e7304b
sathiyaseelan <ssstarlet@gmail.com>
2016-06-08 06:08:23 +0000

    Added Seed file contents to populate the tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# ruby encoding: utf-8
+
+User.delete_all
+Friend.delete_all
+
+User.create!(username: "sathya", email: "ssstarlet@gmail.com",password: "password")
+User.create!(username: "divya", email: "dividulcedo@gmail.com",password: "password")
+User.create!(username: "test_user", email: "test_user@example.com",password: "password")
+
+Friend.create!(user_id: 1, friend_user_id: 2)
+Friend.create!(user_id: 1, friend_user_id: 3)
+Friend.create!(user_id: 2, friend_user_id: 1)
+Friend.create!(user_id: 2, friend_user_id: 3)
+
+User.create!(username: "alex", email: "alex@example.com",password: "password")
