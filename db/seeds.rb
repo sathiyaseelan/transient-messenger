c41e9087bad8710e7821ecc152024e4a39e9b9d6
sathiyaseelan <ssstarlet@gmail.com>
2016-06-08 06:44:17 +0000

    Fixed id issue in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@
 User.delete_all
 Friend.delete_all
 
-User.create!(username: "sathya", email: "ssstarlet@gmail.com",password: "password")
-User.create!(username: "divya", email: "dividulcedo@gmail.com",password: "password")
-User.create!(username: "test_user", email: "test_user@example.com",password: "password")
+user1 = User.create!(username: "sathya", email: "ssstarlet@gmail.com",password: "password")
+user2 = User.create!(username: "divya", email: "dividulcedo@gmail.com",password: "password")
+user3 = User.create!(username: "test_user", email: "test_user@example.com",password: "password")
 
-Friend.create!(user_id: 1, friend_user_id: 2)
-Friend.create!(user_id: 1, friend_user_id: 3)
-Friend.create!(user_id: 2, friend_user_id: 1)
-Friend.create!(user_id: 2, friend_user_id: 3)
+Friend.create!(user_id: user1.id, friend_user_id: user2.id)
+Friend.create!(user_id: user1.id, friend_user_id: user3.id)
+Friend.create!(user_id: user2.id, friend_user_id: user1.id)
+Friend.create!(user_id: user2.id, friend_user_id: user3.id)
 
 User.create!(username: "alex", email: "alex@example.com",password: "password")
